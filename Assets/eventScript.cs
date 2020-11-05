6ee9f20ff88eec037403243ce2290da5f01d7e62
Petrus Jarl <60645354+2peppe2@users.noreply.github.com>
2020-11-05 09:50:49 +0100

    Button Test
    den verkar inte känna när man klickar ner den

---
+++ Assets/eventScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+namespace Valve.VR.InteractionSystem.Sample
+{
+
+    [RequireComponent(typeof(AudioSource))]
+
+    public class eventScript : MonoBehaviour
+    {
+        public HoverButton hoverButton;
+        
+        AudioSource audioSource;
+        AudioSource audioData;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            //Fetch the AudioSource from the GameObject
+            
+            audioData = GetComponent<AudioSource>();
+            audioData.Play(0);
+            hoverButton.onButtonDown.AddListener(OnButtonDown);
+
+        }
+
+
+        private void OnButtonDown(Hand hand)
+        {
+            Debug.Log("Button pressed", hoverButton);
+
+
+            audioData.Pause();
+        }
+    }
+}
