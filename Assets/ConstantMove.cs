aab823491ad6102ad3e9f4f2c59bc530f52162ee
Petrus Jarl <60645354+2peppe2@users.noreply.github.com>
2020-12-01 09:06:33 +0100

    Skrapan påbörjad

---
+++ Assets/ConstantMove.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantMove : MonoBehaviour
+{
+    public float speedZ;
+    private float moveZ;
+    public float HowMuchItMove;
+    private float minZ, maxZ;
+    int zDirection = 0;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        minZ = transform.position.z;
+        maxZ = minZ + HowMuchItMove;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 currentPos = transform.position;
+        if (currentPos.z < maxZ)
+        {
+
+            if (zDirection != 2)
+            {
+
+                moveZ = speedZ * Time.deltaTime; 
+                zDirection = 1;
+            }
+
+        }
+        else if (currentPos.x > maxZ)
+        {
+
+            if (zDirection != 1)
+            {
+
+                moveZ = -speedZ * Time.deltaTime; //flyttar minus på X axeln
+                zDirection = 2;
+            }
+
+
+
+        }
+    }
+
+    void Knappklickad (int which)   //0 = grön, 1 = gul, 2 = röd;
+    {
+
+    }
+}
